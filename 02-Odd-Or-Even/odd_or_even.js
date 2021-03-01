2dc7800fcba7b3a5cc1ad048f47329bd7e2ae521
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-01 11:48:00 +0100

    solving 02-odd-or-even

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,45 @@
   // check if the current is even or odd
 
   // report the result to the screen
+
+ // var number = 0;
+ // if (number % 2 == 0 && number < 20) {
+  //  console.log( number +" is even");    
+  //} else  {
+    //console.log( number +" is odd");
+//
+  //}
+  //number++;
+  //return
+
+  for (var number = 0 ; number <= 20 ; number++) {
+    if (number % 2 == 0){
+    console.log( number +" is even"); 
+  }
+  else { console.log( number +" is odd");
+}
+}
+
+// 2nd way
+
+var num = 0;
+while (num <= 20 ) {
+  if (num % 2 == 0){
+    console.log( num +" is even"); 
+  }
+  else { console.log( num +" is odd");
+} 
+ num ++
+}
+
+// 3rd way
+var n = 0;
+do {
+  if (n % 2 == 0){
+    console.log( n +" is even"); 
+  }
+  else { console.log( n +" is odd");
+} 
+ n ++
+} while (n <= 20 );
+
