8a5099f81d08cdca09966aa3e02fbb233a95e51f
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-01 12:40:59 +0100

    solving 03-multiplication-tables

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,18 @@
 // write your code below
+
+for (var number =0 ; number <= 10 ; number++) {
+   var result = number * 9;
+    console.log(number + " * 9 = "+ result);
+}
+
+// 2nd request
+function number (num, n) {
+    var num = prompt("write your number");
+    for (var n = 0 ; n <= 10 ; n++) {
+     var result = num * n;
+        console.log(num + " * " + n + " = "+result);
+        window.alert(num + " * " + n + " = "+result);
+ }
+}
+
+number();
